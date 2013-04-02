fcba650255eb0ceaf91c8d0fe7990af669d23757
Costas Kirgoussios <ckir1508@hotmal.com>
2013-04-03 01:22:03 +0300

    Upload

--- package.js
+++ package.js
@@ -2,12 +2,8 @@ Package.describe({
   summary: "Jquery Globalize repackaged for Meteor"
 });
 
-Npm.depends({
-  "globalize": "0.1.1",
-  "underscore.string": "2.3.1"
-});
-
 Package.on_use(function (api) {
-  api.add_files('server.js', 'server');
+  api.add_files('lib/globalize.js', ['client', 'server']);
+  api.add_files('lib/cultures/globalize.cultures.js', 'server');
 });
 
